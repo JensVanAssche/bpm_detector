db99017f0ad366514a1f616088038cb1316685a6
JensVanAssche <jens.va@hotmail.com>
2019-01-15 13:33:23 +0100

    init

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ app.post('/', function (req, res) {
     console.log(req.body.input);
     
     (async () => {
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox', '--disable-setuid-sandbox']
-        });
+        const browser = await puppeteer.launch();
         const page = await browser.newPage();
         await page.goto('https://my-free-mp3s.com/en');
         page.type("#query", req.body.input);
