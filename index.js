42ef5dac1b3c87355e1d8cfcd9f8345c578ef25b
JensVanAssche <jens.va@hotmail.com>
2019-01-15 13:44:18 +0100

    init

--- index.js
+++ index.js
@@ -24,16 +24,18 @@ app.post('/', function (req, res) {
         });
         const page = await browser.newPage();
         await page.goto('https://my-free-mp3s.com/en');
-        page.type("#query", req.body.input);
+        await page.type("#query", req.body.input);
         await page.waitFor(100);
-        page.click("button.search");
+        await page.click("button.search");
         await page.waitFor(3000);
-        page.click("li.list-group-item:first-child span.play");
+        await page.click("li.list-group-item:first-child span.play");
         await page.waitFor(100);
-        page.click("li.list-group-item:first-child span.play");
+        await page.click("li.list-group-item:first-child span.play");
         var songUrl = await page.$eval("div.jp-jplayer audio", el => el.src);
         var artist = await page.$eval("li.list-group-item a#navi", el => el.innerHTML);
         var songName = await page.$eval("li.list-group-item a#navi:last-child", el => el.innerHTML);
+
+        console.log("song url found");
     
         await browser.close();
 
