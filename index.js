98b9609b3191aa24e99eefbc71d9eb09502a8601
JensVanAssche <jens.vanassche@student.kdg.be>
2019-08-28 16:40:53 +0200

    a

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ app.post("/", function(req, res) {
 
     (async () => {
       const browser = await puppeteer.launch({
-        headless: true
+        args: ["--no-sandbox"]
       });
       const page = await browser.newPage();
       await page.goto("https://my-free-mp3s.com/");
