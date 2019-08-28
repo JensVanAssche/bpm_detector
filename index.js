fc8a667ebf26aacce7e11fdbf99ca71c205c1a8e
JensVanAssche <jens.vanassche@student.kdg.be>
2019-08-28 14:07:57 +0200

    fix

--- index.js
+++ index.js
@@ -1,64 +1,72 @@
-var express = require('express');
-var puppeteer = require('puppeteer');
-var bodyParser = require('body-parser');
+var express = require("express");
+var puppeteer = require("puppeteer");
+var bodyParser = require("body-parser");
 var app = express();
 
-app.use(bodyParser.urlencoded({
+app.use(
+  bodyParser.urlencoded({
     extended: true
-}));
+  })
+);
 app.use(bodyParser.json());
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
-app.use(express.static(__dirname + '/'));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
+app.use(express.static(__dirname + "/"));
 
-
-
-app.get('/', function (req, res) {
-    res.render(__dirname+'/index.html');
+app.get("/", function(req, res) {
+  res.render(__dirname + "/index.html");
 });
 
-app.post('/', function (req, res) {
-    // check for user input
-    if (req.body.input == "") {
-        console.log("error: no user input")
-        res.render(__dirname+'/index.html');
-        res.end();
-    }
-    else {
-        console.log("searching song...");
-        console.log(req.body.input);
-        
-        (async () => {
-            const browser = await puppeteer.launch({
-                args: ['--no-sandbox']
-            });
-            const page = await browser.newPage();
-            await page.goto('https://my-free-mp3s.com/en');
-            await page.type("#query", req.body.input);
-            await page.waitFor(100);
-            await page.click("button.search");
-            await page.waitForSelector("li.list-group-item");
-            await page.click("li.list-group-item:first-child span.play");
-            await page.waitFor(100);
-            await page.click("li.list-group-item:first-child span.play");
-            var songUrl = await page.$eval("div.jp-jplayer audio", el => el.src);
-            var artist = await page.$eval("li.list-group-item a#navi", el => el.innerHTML);
-            var songName = await page.$eval("li.list-group-item a#navi:last-child", el => el.innerHTML);
-    
-            console.log("song url found");
-        
-            await browser.close();
-    
-            res.render(__dirname+'/tempo.html', {songUrl:songUrl, artist:artist, songName:songName});
-        })();
-    }
+app.post("/", function(req, res) {
+  // check for user input
+  if (req.body.input == "") {
+    console.log("error: no user input");
+    res.render(__dirname + "/index.html");
+    res.end();
+  } else {
+    console.log("searching song...");
+    console.log(req.body.input);
+
+    (async () => {
+      const browser = await puppeteer.launch({
+        args: ["--no-sandbox"]
+      });
+      const page = await browser.newPage();
+      await page.goto("https://my-free-mp3s.com/");
+      await page.type("#query", req.body.input);
+      await page.waitFor(100);
+      await page.click("button.search");
+      await page.waitForSelector("li.list-group-item");
+      await page.click("li.list-group-item:first-child span.play");
+      await page.waitFor(100);
+      await page.click("li.list-group-item:first-child span.play");
+      var songUrl = await page.$eval("div.jp-jplayer audio", el => el.src);
+      var artist = await page.$eval(
+        "li.list-group-item a#navi",
+        el => el.innerHTML
+      );
+      var songName = await page.$eval(
+        "li.list-group-item a#navi + a",
+        el => el.innerHTML
+      );
+
+      console.log("song url found");
+
+      await browser.close();
+
+      res.render(__dirname + "/tempo.html", {
+        songUrl: songUrl,
+        artist: artist,
+        songName: songName
+      });
+    })();
+  }
 });
 
 app.listen(process.env.PORT || 5000);
 
 if (process.env.PORT == undefined) {
-    console.log("server running on http://localhost:5000");
-}
-else {
-    console.log("server running on port " + process.env.PORT);
+  console.log("server running on http://localhost:5000");
+} else {
+  console.log("server running on port " + process.env.PORT);
 }
